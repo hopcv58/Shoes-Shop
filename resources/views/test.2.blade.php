ce430554bf2909fea8b73bfa3e58e49bb5bbe9fd
hopcv58 <anduin1995@gmail.com>
2017-03-31 03:26:19 +0700

    user seed fixed

--- resources/views/test.2.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 3/30/2017
- * Time: 3:46 PM
- */
--- resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 3/30/2017
- * Time: 3:45 PM
- */
